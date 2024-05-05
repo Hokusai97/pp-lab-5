e0d6bb5e80b0d331a17525b421aeb73ee354ae84
Hokusai97 <rw.rafalwiacek@gmail.com>
2024-05-05 15:41:29 +0200

    Zadanie 4

--- Main.java
+++ Main.java
@@ -4,24 +4,31 @@ import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        Person[] people = new Person[5];
+
+        final int b = 10;
+
         try {
-            new Person("Rafał", 26);
+            people[0] = new Person("John Doe", 30);
+            people[1] = new Person("Rafał Wiącek", 16);
+            people[2] = new Person("John Cooper", 40);
+            people[3] = new Person("James Hetfield", 35);
+            people[4] = new Person("Adam Gontier", 28);
         } catch (Exception ex) {
             System.out.println(ex);
         }
 
-        try {
-            new Person("Rafał", 126); 
-        } catch (Exception ex) {
-            System.out.println(ex);
+        for (int i = 0; i < people.length; i++) {
+            if (people[i] != null) {
+                int age = people[i].getAge();
+                int value = MathUtils.add(age, b);
+
+                String message = "Hello World! This is a test email message! " + people[i].getName() + " your age is: " + value;
+
+                EmailMessenger emailMessenger = new EmailMessenger();
+
+                emailMessenger.sendMessage(message);
+            }
         }
-
-        int result = MathUtils.add(7, 7);
-
-        System.out.println("Results are: " + result);
-
-        EmailMessenger emailMessenger = new EmailMessenger();
-
-        emailMessenger.sendMessage("Hello World! This is a test email message! Results are: " + result);
     }
 }
