3d4d598be10a7fdc5e828fd184d3abbf6f84884f
Hokusai97 <rw.rafalwiacek@gmail.com>
2024-05-05 14:41:29 +0200

    Zadanie 2

--- Main.java
+++ Main.java
@@ -1,15 +1,22 @@
 import company.beans.Person;
-import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Person person = new Person("Rafał", 26);
-            
-            System.out.println("Name: " + person.getName());
-            System.out.println("Age: " + person.getAge());
-        } catch (InvalidAgeException e) {
-            System.out.println("Error: " + e.getMessage());
+            Person person1 = new Person("Rafał", 26);
+            System.out.println("Person 1: " + person1.getName() + ", " + person1.getAge());
+        } catch (Exception ex) {
+            System.out.println(ex);
         }
+
+        try {
+            new Person("Rafał2", 126); 
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        EmailMessenger emailMessenger = new EmailMessenger();
+        emailMessenger.sendMessage("Hello World! This is a test email message!");
     }
 }
